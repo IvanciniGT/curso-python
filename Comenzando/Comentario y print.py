3717ef1759376103e1efa654b93de7d83e477d53
Jose Luis Pintado Lopez <carpix29@gmail.com>
2018-07-10 02:41:23 +0200

    1.2 - Comentario y print basico.

---
+++ Comenzando/Comentario y print.py
@@ -0,0 +1,10 @@
+#Definiendo tipos de variables
+#Define las siguientes variables: entera/int, String/texto, booleana/logica.
+mivariableEdad = 2
+mivariabletexto = "hola mundo"
+mivariablelogica = True
+
+#imprime por pantalla cada una de las variables
+print (mivariableEdad)
+print(mivariabletexto)
+print(mivariablelogica)
